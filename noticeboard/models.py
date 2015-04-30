1557a65522bd10788c0700bfdb14e357c9c85e8a
Ashish Charan Tandi <act.jnv@gmail.com>
2015-04-30 12:52:51 +0530

    Modified Test for Admin site.oh! But failed

--- noticeboard/models.py
+++ noticeboard/models.py
@@ -3,14 +3,14 @@ from django.db import models
 # Create your models here.
 
 class CreateEvent(models.Model):
-	event_name = models.CharField(max_length=200)
-	venue = models.CharField(max_length=20)
-	date_time = models.DateField()
-	seats = models.IntegerField(default = 200)
-	event_description = models.CharField(max_length = 1000)
-	event_instructions = models.CharField(max_length = 500)
-	option_website_link = models.CharField(max_length = 100)
-	option_contact_info =  models.CharField(max_length = 100)
+    event_name = models.CharField(max_length=200)
+    venue = models.CharField(max_length=20)
+    date_time = models.DateField()
+    seats = models.IntegerField(default = 200)
+    event_description = models.CharField(max_length = 1000)
+    event_instructions = models.CharField(max_length = 500)
+    option_website_link = models.CharField(max_length = 100)
+    option_contact_info =  models.CharField(max_length = 100)
 
-	def __str__(self):
-		return self.event_name + " | " +self.venue+ " | "+ str(self.seats)
+    def __str__(self):
+	return self.event_name + " | " +self.venue+ " | "+ str(self.seats)
